3f2ee84bc17fe01b72e5484f56e885091845a8c8
Mason Rhodes <mason.rhodes@outlook.com>
2023-05-16 21:51:27 +0000

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { createNewClient } from "./client"
+
+if (!process.env.DISCORD_BOT_TOKEN) {
+  console.error("No token provided. Please set the TOKEN environment variable.")
+  process.exit(1)
+}
+if (!process.env.CLIENT_ID) {
+  console.error("No client ID provided. Please set the CLIENT_ID environment variable.")
+  process.exit(1)
+}
+
+createNewClient()
+// restart the bot on failure
+process.on('unhandledRejection', error => {
+  console.error('Unhandled promise rejection:', error);
+  createNewClient()
+});
